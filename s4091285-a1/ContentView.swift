2c0da52d1dedb8b6f93afad273cccc883adfaf2e
binny <s4091285@student.rmit.edu.au>
2025-08-07 16:00:04 +1000

    Initial Commit

---
+++ s4091285-a1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  s4091285-a1
+//
+//  Created by Binyam Sisay on 7/8/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ s4091285-a1/s4091285_a1App.swift
@@ -0,0 +1,17 @@
+//
+//  s4091285_a1App.swift
+//  s4091285-a1
+//
+//  Created by Binyam Sisay on 7/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct s4091285_a1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
